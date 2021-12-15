bc2bae6dbce7ca7345e1f824f6046b2c2843f144
corey <corey@192.168.254.23>
2021-12-15 02:52:12 -0500

    update

---
+++ triangleAreaCalc/src/triangleAreaCalc/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package triangleAreaCalc;
+
+/**
+ * @author corey
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
