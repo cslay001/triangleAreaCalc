56403211304e9d73ae61ecfe2b3a92e36e5b53ad
corey <corey@192.168.254.23>
2021-12-15 03:56:15 -0500

    update

--- triangleAreaCalc/src/triangleAreaCalc/triangleAreaCalc.java
+++ triangleAreaCalc/src/triangleAreaCalc/triangleAreaCalc.java
@@ -16,31 +16,32 @@ public class triangleAreaCalc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("let's calculate the area of a triangle!");
-		System.out.println("<press spacebar to continue>");
 		
 		Scanner input = new Scanner(System.in);
 		System.out.println("                                        ");
 		
-		System.out.println("**************************************************");
+		System.out.println("*********************************************************");
 		System.out.println("                                        ");
 		
 		System.out.println("please input the base of the triangle (in inches)");
 		double base = input.nextDouble();
 		
-		while (base <=0) {
-			System.out.println("That's invalid. Please input the base of the triangle (in inches)");
-		}
+			while (base <=0) {
+				System.out.println("That's invalid. Please input the base of the triangle (in inches)");
+			}
 		
 		System.out.println("Please input the height of the triangle (in inches)");
 		double height = input.nextDouble();
 		
-		while (height <=0) {
-			System.out.println("That's invalid. Please input the height of the triangle (in inches)");
-		}
+			while (height <=0) {
+				System.out.println("That's invalid. Please input the height of the triangle (in inches)");
+			}
 		
 		double area = (base * height) / 2;
 		
-		System.out.println("**************************************************");
+		System.out.println(" ");
+		System.out.println("*********************************************************");
+		System.out.println(" ");
 		System.out.println("the area of this triangle is " + area);
 		
 
